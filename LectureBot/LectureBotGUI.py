7641f6a6af55bfd2c59916a422d03674ecd98ce3
DorgeloK <155837067+DorgeloK@users.noreply.github.com>
2025-06-10 15:38:28 -0400

    Correcting all the suggestions (grey lines)

--- LectureBot/LectureBotGUI.py
+++ LectureBot/LectureBotGUI.py
@@ -1,7 +1,7 @@
-'''
+"""
     Description: This initializes the GUI and assigns the functions to each button
     Return: NONE
-'''
+"""
 from tkinter import *
 from pathlib import Path
 from tkinter import filedialog
@@ -16,21 +16,33 @@ import threading
 class GUI:
 
     def __init__(self):
-        '''
+        """
             Description: This initializes the GUI
             Return: NONE
-        '''
-        self.main = Tk() # Creating GUI base
+        """
+        self.main = Tk()  # Creating GUI base
         self.savingFile = None
-        self.saveButton = None
         self.filename = None
         self.lectureName = None
         self.box = None
         self.box2 = None
+        self.box3 = None
         self.choosing_lang = None
-        self.name = None
         self.dest_lang = ""
         self.src_lang = ""
+        self.downloadButton = None
+        self.downloadButtonImg = None
+        self.pancakeButton = None
+        self.pancakeButtonImg = None
+        self.audioButton = None
+        self.audioButtonImg = None
+        self.transcriptButton = None
+        self.saveButton = None
+        self.saveButtonImg = None
+        self.audiofilebutton = None
+        self.uploadButtonImg = None
+        self.stop_thread = None
+        self.thread = None
 
         self.main.geometry("900x900")
         self.main.configure(background="#FFA580")
@@ -70,69 +82,69 @@ class GUI:
         # This is so you can edit and save the text that was outputted.
         self.txt_file = "output.txt"
         self.curr_file = "output.txt"
-        # For the drop down menus for the Translation
+        # For the dropdown menus for the Translation
         self.clicked = None
         self.clicked2 = None
 
         self.run()
 
     def run(self):
-        '''
+        """
             Description: This function runs the GUI
             Return: NONE
-        '''
+        """
         self.main.mainloop()
 
     def placeButtons(self):
-        '''
+        """
             Description: This function places the buttons onto the GUI and assigns functionality
             Return: NONE
-        '''
+        """
 
         # Assigning picture to each button
-        self.downloadButtonImg = PhotoImage(file = 'download.png')
-        self.pancakeButtonImg = PhotoImage(file = 'pancake.png')
-        self.audioButtonImg = PhotoImage(file = 'mic.png')
+        self.downloadButtonImg = PhotoImage(file='download.png')
+        self.pancakeButtonImg = PhotoImage(file='pancake.png')
+        self.audioButtonImg = PhotoImage(file='mic.png')
         self.saveButtonImg = PhotoImage(file='save_button.png')
-        self.stopButtonImg = PhotoImage(file='stop_button.png')
         self.uploadButtonImg = PhotoImage(file='upload.png')
 
         # Adding picture to each button and assigning function
-        self.downloadButton = Button(self.main, image = self.downloadButtonImg, command = self.downloadPress)
-        self.pancakeButton = Button(self.main, image = self.pancakeButtonImg, command = self.pancakePress)
-        self.audioButton = Button(self.main, image = self.audioButtonImg, command = self.audioPress)
-        self.transcriptButton = Button(self.main, text = "T", command = self.translatePress)
+        self.downloadButton = Button(self.main, image=self.downloadButtonImg, command=self.downloadPress)
+        self.pancakeButton = Button(self.main, image=self.pancakeButtonImg, command=self.pancakePress)
+        self.audioButton = Button(self.main, image=self.audioButtonImg, command=self.audioPress)
+        self.transcriptButton = Button(self.main, text="T", command=self.translatePress)
         self.saveButton = Button(self.main, image=self.saveButtonImg, command=self.savePress)
-        self.audiofilebutton = Button(self.main, image=self.uploadButtonImg, command = self.audiofilepress)
+        self.audiofilebutton = Button(self.main, image=self.uploadButtonImg, command=self.audiofilepress)
 
         # Places transcript button
-        self.transcriptButton.config(font = ("Times New Roman", 21, "bold"), background = "white", width = "1", height = "1")  # performs callback of function
-        self.transcriptButton.place(x = 731, y = 0)
+        # performs callback of function
+        self.transcriptButton.config(font=("Times New Roman", 21, "bold"), background="white", width="1", height="1")
+        self.transcriptButton.place(x=731, y=0)
 
         # Places download button
-        self.downloadButton.config(width = "44", height = "50")
-        self.downloadButton.place(x = 850, y = 0)
+        self.downloadButton.config(width="44", height="50")
+        self.downloadButton.place(x=850, y=0)
 
         # Places pancake button
-        self.pancakeButton.config(width = "50", height = "50")
-        self.pancakeButton.place(x = 0, y = 0)
+        self.pancakeButton.config(width="50", height="50")
+        self.pancakeButton.place(x=0, y=0)
 
         # Places audio button
-        self.audioButton.config(width = "35", height = "50")
-        self.audioButton.place(x = 690, y = 0)
+        self.audioButton.config(width="35", height="50")
+        self.audioButton.place(x=690, y=0)
 
         self.saveButton.config(width="41", height="50")
         self.saveButton.place(x=758, y=0)
 
         # Places audiofile button
-        self.audiofilebutton.config(font = ("Times New Roman",7,"bold"), background = "white", width = "40", height = "50")
-        self.audiofilebutton.place(x=805,y=0)
+        self.audiofilebutton.config(font=("Times New Roman", 7, "bold"), background="white", width="40", height="50")
+        self.audiofilebutton.place(x=805, y=0)
 
     def translatePress(self):
-        '''
+        """
             Description: Translate button pressed-> button prompts the audio to text conversion
             Return: NONE
-        '''
+        """
         # Have a screen pop up that asks which file to translate and which language (dest_lang)
 
         self.choosing_lang = Toplevel()  # Creating pop up window
@@ -164,7 +176,7 @@ class GUI:
                               'Russian', 'Samoan', 'Scots gaelic', 'Serbian', 'Sesotho', 'Shona', 'Sindhi', 'Sinhala',
                               'Slovak', 'Slovenian', 'Somali', 'Spanish', 'Sundanese', 'Swahili', 'Swedish', 'Tajik',
                               'Tamil', 'Telugu', 'Thai', 'Turkish', 'Ukrainian', 'Urdu', 'Uyghur', 'Uzbek',
-                              'Vietnamese', 'Welsh', 'Xhosa','Yiddish', 'Yoruba', 'Zulu')
+                              'Vietnamese', 'Welsh', 'Xhosa', 'Yiddish', 'Yoruba', 'Zulu')
         """self.box = Entry(self.choosing_lang, width=60)"""
         self.box.place(x=25, y=90)
 
@@ -176,20 +188,20 @@ class GUI:
         self.clicked2.set("Spanish")
 
         self.box2 = OptionMenu(self.choosing_lang, self.clicked2, 'Afrikaans', 'Albanian', 'Amharic', 'Arabic',
-                              'Armenian', 'Azerbaijani', 'Basque', 'Belarusian', 'Bengali', 'Bosnian', 'Bulgarian',
-                              'Catalan', 'Cebuano', 'Chichewa', 'Chinese (simplified)', 'Chinese (traditional)',
-                              'Corsican', 'Croatian', 'Czech', 'Danish', 'Dutch', 'English', 'Esperanto', 'Estonian',
-                              'Filipino', 'Finnish', 'French', 'Frisian', 'Galician', 'Georgian', 'German', 'Greek',
-                              'Gujarati', 'Haitian creole', 'Hausa', 'Hawaiian', 'Hebrew', 'Hindi', 'Hmong',
-                              'Hungarian', 'Icelandic', 'Igbo', 'Indonesian', 'Irish', 'Italian', 'Japanese',
-                              'Javanese', 'Kannada', 'Kazakh', 'Khmer', 'Korean', 'Kurdish (Kurmanji)', 'Kyrgyz', 'Lao',
-                              'Latin', 'Latvian', 'Lithuanian', 'Luxembourgish', 'Macedonian', 'Malagasy', 'Malay',
-                              'Malayalam', 'Maltese', 'Maori', 'Marathi', 'Mongolian', 'Myanmar (burmese)', 'Nepali',
-                              'Norwegian', 'Odia', 'Pashto', 'Persian', 'Polish', 'Portuguese', 'Punjabi', 'Romanian',
-                              'Russian', 'Samoan', 'Scots gaelic', 'Serbian', 'Sesotho', 'Shona', 'Sindhi', 'Sinhala',
-                              'Slovak', 'Slovenian', 'Somali', 'Spanish', 'Sundanese', 'Swahili', 'Swedish', 'Tajik',
-                              'Tamil', 'Telugu', 'Thai', 'Turkish', 'Ukrainian', 'Urdu', 'Uyghur', 'Uzbek',
-                              'Vietnamese', 'Welsh', 'Xhosa', 'Yiddish', 'Yoruba', 'Zulu')
+                               'Armenian', 'Azerbaijani', 'Basque', 'Belarusian', 'Bengali', 'Bosnian', 'Bulgarian',
+                               'Catalan', 'Cebuano', 'Chichewa', 'Chinese (simplified)', 'Chinese (traditional)',
+                               'Corsican', 'Croatian', 'Czech', 'Danish', 'Dutch', 'English', 'Esperanto', 'Estonian',
+                               'Filipino', 'Finnish', 'French', 'Frisian', 'Galician', 'Georgian', 'German', 'Greek',
+                               'Gujarati', 'Haitian creole', 'Hausa', 'Hawaiian', 'Hebrew', 'Hindi', 'Hmong',
+                               'Hungarian', 'Icelandic', 'Igbo', 'Indonesian', 'Irish', 'Italian', 'Japanese',
+                               'Javanese', 'Kannada', 'Kazakh', 'Khmer', 'Korean', 'Kurdish (Kurmanji)', 'Kyrgyz',
+                               'Lao', 'Latin', 'Latvian', 'Lithuanian', 'Luxembourgish', 'Macedonian', 'Malagasy',
+                               'Malay', 'Malayalam', 'Maltese', 'Maori', 'Marathi', 'Mongolian', 'Myanmar (burmese)',
+                               'Nepali', 'Norwegian', 'Odia', 'Pashto', 'Persian', 'Polish', 'Portuguese', 'Punjabi',
+                               'Romanian', 'Russian', 'Samoan', 'Scots gaelic', 'Serbian', 'Sesotho', 'Shona', 'Sindhi',
+                               'Sinhala', 'Slovak', 'Slovenian', 'Somali', 'Spanish', 'Sundanese', 'Swahili', 'Swedish',
+                               'Tajik', 'Tamil', 'Telugu', 'Thai', 'Turkish', 'Ukrainian', 'Urdu', 'Uyghur', 'Uzbek',
+                               'Vietnamese', 'Welsh', 'Xhosa', 'Yiddish', 'Yoruba', 'Zulu')
         """self.box2 = Entry(self.choosing_lang, width=60)"""
         self.box2.place(x=25, y=140)
 
@@ -204,11 +216,11 @@ class GUI:
         nextButton.place(x=180, y=230)
 
     def done(self):
-        '''
+        """
             Description: This button allows the program to access the source and destination languages. Then it calls
             translate() to translate the text. Finally, it destroys the window.
             Return: NONE
-        '''
+        """
         LANGUAGES = dict({'afrikaans': 'af', 'albanian': 'sq', 'amharic': 'am', 'arabic': 'ar', 'armenian': 'hy',
                           'azerbaijani': 'az', 'basque': 'eu', 'belarusian': 'be', 'bengali': 'bn', 'bosnian': 'bs',
                           'bulgarian': 'bg', 'catalan': 'ca', 'cebuano': 'ceb', 'chichewa': 'ny',
@@ -230,7 +242,7 @@ class GUI:
                           'spanish': 'es', 'sundanese': 'su', 'swahili': 'sw', 'swedish': 'sv', 'tajik': 'tg',
                           'tamil': 'ta', 'telugu': 'te', 'thai': 'th', 'turkish': 'tr', 'ukrainian': 'uk', 'urdu': 'ur',
                           'uyghur': 'ug', 'uzbek': 'uz', 'vietnamese': 'vi', 'welsh': 'cy', 'xhosa': 'xh',
-                          'yiddish': 'yi', 'yoruba': 'yo', 'zulu': 'zu',})
+                          'yiddish': 'yi', 'yoruba': 'yo', 'zulu': 'zu'})
         self.src_lang = LANGUAGES[self.clicked.get().lower()]
         print(self.src_lang)
         self.dest_lang = LANGUAGES[self.clicked2.get().lower()]
@@ -239,10 +251,10 @@ class GUI:
         self.choosing_lang.destroy()
 
     def translate(self):
-        '''
+        """
             Description: This translates the file.
             Return: NONE
-        '''
+        """
         t = Translate(self.src_lang, self.dest_lang, self.curr_file, self.text_box)
         asyncio.run(t.output(self.text_box.get(1.0, END)))
         t.display()
@@ -252,10 +264,10 @@ class GUI:
         self.fileList.append(t.file_name)
 
     def audioPress(self):
-        '''
+        """
             Description: Audio button pressed-> button prompts audio to begin recording
             Return: NONE
-        '''
+        """
         print("audio")
         self.stop_thread = threading.Event()
         a = Audio_to_Text(self.text_box)
@@ -263,12 +275,12 @@ class GUI:
         self.thread.start()
 
     def savePress(self):
-        '''
+        """
             Description: This button allows you to save the changes you made in the text box.
             Return: None
-        '''
+        """
 
-        print("Saving...") # testing GUI buttons go to correct task
+        print("Saving...")  # testing GUI buttons go to correct task
         if isinstance(self.txt_file, str):
             self.curr_file = self.txt_file
         self.txt_file = open(f'{self.curr_file}', "w")
@@ -276,53 +288,53 @@ class GUI:
         self.txt_file.close()
 
     def pancakePress(self):
-        '''
+        """
             Description: Pancake button pressed-> pop up window appears to show what files have been
                         created and saved so far
             Return: NONE
-        '''
+        """
 
-        if (self.isPanelVisible == False): # If the panel is not shown
-            self.pancakePanel.place(x = 0, y = 50)  # Show the panel
+        if self.isPanelVisible is False:  # If the panel is not shown
+            self.pancakePanel.place(x=0, y=0)  # Show the panel
 
             y = 0
             for item in self.fileList:      # List all the file created
                 label = Label(self.pancakePanel, text=item, font=("Times New Roman", 12, "bold"), background="orange")
-                label.place(x = 18, y = 40 + y)
+                label.place(x=18, y=40 + y)
                 y += 30
 
             self.isPanelVisible = True      # Change state to panel shown
 
         else:   # If the panel is shown
-            self.pancakePanel.place(x = -200, y = 50) # Make the panel disappear
+            self.pancakePanel.place(x=-200, y=50)  # Make the panel disappear
             self.isPanelVisible = False     # Change state to panel not shown
 
     def downloadPress(self):
-        '''
+        """
             Description: Download button pressed-> pop up window appears to prompt user
                         to save file by a specified name.
             Return: NONE
-        '''
+        """
 
         self.savingFile = Toplevel()    # Creating pop up window
         self.savingFile.title("Naming Lecture File")
         self.savingFile.geometry("200x100")
 
-        label = Label(self.savingFile, text = "File saves to downloads folder", font = ("Times New Roman", 10))
-        label.place(x = 18, y = 10)             # Always saving files to downloads folder
+        label = Label(self.savingFile, text="File saves to downloads folder", font=("Times New Roman", 10))
+        label.place(x=18, y=10)             # Always saving files to downloads folder
 
-        self.saveButton = Button(self.savingFile, text = "Save", command = self.getFilename)
-        self.saveButton.place(x = 85, y = 70)  # Creating save button on pop up window, this button saves the file to downloads folder
+        self.saveButton = Button(self.savingFile, text="Save", command=self.getFilename)
+        # Creating save button on pop up window, this button saves the file to downloads folder
+        self.saveButton.place(x=85, y=70)
 
-        self.filename = Entry(self.savingFile, width = 25)
-        self.filename.place(x = 25, y = 30)  # Creating text box for user to enter a file name
-
+        self.filename = Entry(self.savingFile, width=25)
+        self.filename.place(x=25, y=30)  # Creating text box for user to enter a file name
 
     def getFilename(self):
-        '''
+        """
             Description: Getting file name from GUI text box
             Return: NONE
-        '''
+        """
         self.lectureName = self.filename.get() + ".txt"
         self.fileList.append(self.lectureName)
 
@@ -330,10 +342,10 @@ class GUI:
         self.savingFile.destroy()
 
     def saveFile(self):
-        '''
+        """
             Description: Saving file to downloads folder
             Return: NONE
-        '''
+        """
 
         downloads = Path.home() / "Downloads"
         file_path = downloads / self.lectureName
@@ -348,14 +360,15 @@ class GUI:
             output.write("")
 
     def saveTranslated(self):
-        '''
+        """
             Description: Saving translated file to downloads folder and then clearing output.txt, as well.
             Return: NONE
-        '''
+        """
         downloads = Path.home() / "Downloads"
         file_path = downloads / self.lectureName
 
-        with open("translated_output.txt", "r", encoding='utf-8') as output:  # Copying contents of the output file into the file the user created
+        # Copying contents of the output file into the file the user created
+        with open("translated_output.txt", "r", encoding='utf-8') as output:
             content = output.read()
 
         with open(file_path, "w", encoding='utf-8') as new_file:
@@ -373,31 +386,31 @@ class GUI:
         trancribes it into text.
         :return: None
         """
-        file_path = filedialog.askopenfilename(title="Select audio file",filetypes=[("Audio Files", "*.wav")])
+        file_path = filedialog.askopenfilename(title="Select audio file", filetypes=[("Audio Files", "*.wav")])
         if file_path:
             transcriber = AudioFileToText(file_path)
             text = transcriber.transcribe()
 
             if text:
-                with open(self.file,"a") as f:
+                with open(self.file, "a") as f:
                     f.write(text + "\n")
                 self.text_box = Text(self.main, width=70, height=20, font=("Times New Roman", 12))
                 self.text_box.place(x=200, y=120)
-                self.text_box.insert(END,text)
+                self.text_box.insert(END, text)
 
 
-class Translate():
-    '''
+class Translate:
+    """
         Description: This class helps us translate the text and output it into the text_box in the GUI.
-    '''
+    """
     def __init__(self, src_lang, dest_lang, file_name, text_box):
-        '''
+        """
             Parameters:
                 src_lang = the language we are translating from.
                 dest_lang = the language we want to translate to.
                 file_name = the name of the file that is in the text box.
                 text_box = the textbox in the GUI itself.
-        '''
+        """
         self.src_lang = src_lang
         self.dest_lang = dest_lang
         self.file = file_name
@@ -424,10 +437,10 @@ class Translate():
         trans_file.close()
 
     def display(self):
-        '''
+        """
             Description: Inserts the translated text into the GUI.
             Return: NONE
-        '''
+        """
         self.text_box.delete(1.0, END)
         f = open(f'{self.file_name}', "r", encoding="utf-8", errors='ignore')
         reading = f.read()
@@ -441,10 +454,11 @@ class Audio_to_Text(threading.Thread):
         super(Audio_to_Text, self).__init__()
         self.recog = sr.Recognizer()
         self.text_box = text_box
+        self.file = None
 
     def listen(self):
         with sr.Microphone() as source:
-            while (1):
+            while 1:
                 try:
                     self.recog.adjust_for_ambient_noise(source, duration=0.5)
                     audio = self.recog.listen(source, phrase_time_limit=1200)
@@ -476,10 +490,10 @@ class Audio_to_Text(threading.Thread):
 
 class LectureBot(GUI):
     def build(self):
-        '''
+        """
             Description: Builds the GUI
             Return: NONE
-        '''
+        """
         return self
 
 
